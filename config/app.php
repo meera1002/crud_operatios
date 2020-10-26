8061bbcc681793556dc00d2b742dd5a240e0f3a6
Meera Sebastian <meera.sebastian@citrix.com>
2020-10-26 13:11:17 +0530

    Appview

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Curd Application'),
+    'name' => env('APP_NAME', 'Crud Application'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> Curd Application</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
